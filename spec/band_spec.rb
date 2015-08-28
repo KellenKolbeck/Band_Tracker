6e117b14f4fb24a1187d605080702257c2044faf
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-08-28 12:56:57 -0700

    Add .create instead of .new to pass new spec for capitalizing a band and venue name

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,7 +2,12 @@ require("spec_helper")
 
 describe(Band) do
   it("will validate prescence of name") do
-    category = Band.new({:name => ""})
-    expect(category.save()).to(eq(false))
+    band = Band.new({:name => ""})
+    expect(band.save()).to(eq(false))
+  end
+
+  it("capitalizes the name of a band") do
+    band = Band.create(:name => "cream")
+    expect(band.name()).to(eq("Cream"))
   end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,7 +2,11 @@ require("spec_helper")
 
 describe(Venue) do
   it("will validate prescence of name") do
-    category = Venue.new({:name => ""})
-    expect(category.save()).to(eq(false))
+    venue = Venue.new({:name => ""})
+    expect(venue.save()).to(eq(false))
+  end
+  it("capitalizes the name of a venue") do
+    venue = Venue.create(:name => "fillmore")
+    expect(venue.name()).to(eq("Fillmore"))
   end
 end
